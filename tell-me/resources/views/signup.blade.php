ab4b44c8b24fbbb4aa1ac09709b3be2fe110f10d
pelusb <peluissaraiva@gmail.com>
2024-10-08 16:40:16 -0300

    - Adicionado csrf no formulário de cadastre-se - Adicionado apresentação dos erros de validação no formulário de cadastre-se

--- tell-me/resources/views/signup.blade.php
+++ tell-me/resources/views/signup.blade.php
@@ -4,11 +4,32 @@
     <title>Cadastre-se</title>
 </head>
 <body>
+@if($errors->any())
+    <div>
+        <ul>
+            @foreach($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+
+        </ul>
+    </div>
+@endif
     <form action="{{ route('register') }}" method="POST">
+        @csrf
 
         <div>
             <label for="name">Nome: </label>
             <input type="text" name="name" id="name" value="{{ old('name') }}">
+        </div>
+
+        <div>
+            <label for="email">E-mail:</label>
+            <input type="email" name="email" id="email" value="{{ old('email') }}" required>
+        </div>
+
+        <div>
+            <label for="password">Senha:</label>
+            <input type="password" name="password" id="password" required>
         </div>
 
 
